09ba4d88a0d13f9574e85e2b640dd48e71fff32b
André Rodrigues <andre360.ars@gmail.com>
2022-01-25 20:58:54 -0300

    Requisito 4 - Cria duas funções que, verificam se certo id é de um manager, e retornam lista de pessoas gerenciadas por eles

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managersIds = [stephanieId, olaId, burlId];
 
 function isManager(id) {
-  // seu código aqui
+  return managersIds.some((ids) => ids === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const relatedEmployees = [];
+  const { employees } = data;
+
+  if (isManager(managerId)) {
+    const filtred = employees.filter(({ managers }) => managers.includes(managerId));
+    filtred.forEach(({ firstName, lastName }) => relatedEmployees.push(`${firstName} ${lastName}`));
+
+    return relatedEmployees;
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
