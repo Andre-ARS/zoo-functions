c26b638920d2efb13f8395e37c4dc67d1977914a
André Rodrigues <andre360.ars@gmail.com>
2022-01-25 17:19:09 -0300

    Requisito 2 - Cria função que, retorna true ou false, se todos os animais, de uma dada especie, tem uma dada idade minima

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
+  const { species } = data;
+
+  const filtred = species.filter((specie) => animal === specie.name)[0].residents;
+
+  const validation = filtred.every((zooAnimal) =>  {
+    const { age: animalAge } = zooAnimal;
+
+    return animalAge >= age;
+  });
+
+  return validation;
 }
 
 module.exports = getAnimalsOlderThan;
