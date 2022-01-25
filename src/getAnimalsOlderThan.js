4b00b78d8898656cc23dc69faf14305d7407cb6f
André Rodrigues <andre360.ars@gmail.com>
2022-01-25 16:27:52 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+
+  species.filter((specie) => animal === specie.name);
+  
+  //   const { residents } = specie;
 }
 
 module.exports = getAnimalsOlderThan;
