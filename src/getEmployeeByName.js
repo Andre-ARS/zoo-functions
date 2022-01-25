e42fa4faa22894e1d7c577d70af7af67a26af87e
André Rodrigues <andre360.ars@gmail.com>
2022-01-25 18:27:07 -0300

    linterFix

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const {employees} = data;
+  const { employees } = data;
   if (employeeName === undefined) {
     return {};
   }
