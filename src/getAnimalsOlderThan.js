3b9794988155396da851dbe37fa86ea5f122958c
André Rodrigues <andre360.ars@gmail.com>
2022-01-25 16:31:51 -0300

    Requisito 1 - lintFix

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,10 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const { species } = data;
-
-  species.filter((specie) => animal === specie.name);
   
-  //   const { residents } = specie;
+  species.filter((specie) => animal === specie.name);
+
+  // const { residents } = specie;
 }
 
 module.exports = getAnimalsOlderThan;
