b712c595a1de7924d3868757939d9aa6ddb9059c
André Rodrigues <andre360.ars@gmail.com>
2022-01-25 15:56:14 -0300

    Requisito 1 - Cria função que, retorna um array com 1 ou mais objetos, conforme id passado como parametro

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  const obtainedSpecie = []
+
+  species.forEach(specie => {
+    const { id } = specie;
+
+    if (id === ids[0] || id === ids[1]) {
+      obtainedSpecie.push(specie);
+    }
+  });
+
+  return obtainedSpecie;
 }
 
 module.exports = getSpeciesByIds;
