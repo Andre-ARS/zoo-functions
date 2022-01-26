11744b370c670747de415b679c8e49e9a1d417e9
André Rodrigues <andre360.ars@gmail.com>
2022-01-26 10:44:21 -0300

    LinterFix

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,14 +5,15 @@ const { species } = data;
 const countByName = ({ specie }) => species.find(({ name }) => name === specie).residents.length;
 
 const countBySex = ({ specie, sex }) => {
- return species.find(({ name }) => name === specie).residents.filter(({ sex: sexo }) => sexo === sex).length;
-}
+  const filtred = species.find(({ name }) => name === specie).residents;
+  return filtred.filter(({ sex: sexo }) => sexo === sex).length;
+};
 
 function countAnimals(animal) {
   const counter = {};
 
   if (typeof animal !== 'object') {
-    species.forEach(({ name, residents }) => {counter[name] = residents.length});
+    species.forEach(({ name, residents }) => { counter[name] = residents.length; });
 
     return counter;
   }
