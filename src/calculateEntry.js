6ef161236bea5cdd198428a4e11639f392bf93a0
André Rodrigues <andre360.ars@gmail.com>
2022-01-26 11:51:20 -0300

    Requisito 6 - Cria função que, retorna soma dos preços das entradas, conforme idade dos visitantes

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+
+  return { child, adult, senior, };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  };
+
+  const { child: childCount, adult: adultcount, senior: seniorCount } = countEntrants(entrants);
+  const { child, adult, senior } = prices;
+
+  return (childCount * child) + (adultcount * adult) + (seniorCount * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
