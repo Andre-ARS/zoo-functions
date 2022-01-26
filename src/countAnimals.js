4de4fbd73b02c60ed4581b0c5f31e79a5b934343
André Rodrigues <andre360.ars@gmail.com>
2022-01-26 10:02:39 -0300

    Requisto 5 - Cria função que, retorna objeto com a contagem de animais de cada especie, se não for passado nenhum nome, caso se passe o nome ou o nome e sexo, retorna contagem dos animais que passam nessas condições

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const counter = {}
+
+  if (typeof animal !== 'object') {
+    species.forEach(({ name, residents }) => counter[name] = residents.length);
+  
+    return counter; 
+  }
+
+  return Object.keys(animal).length === 1 ? countByName(animal) : countBySex(animal);
 }
+
+const countByName = ({ specie }) => species.find(({ name }) => name === specie).residents.length;
+
+const countBySex = ({ specie, sex }) => species.find(({ name }) => name === specie).residents.filter(({ sex:sexo }) => sexo === sex).length;
 
 module.exports = countAnimals;
