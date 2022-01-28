0d56015fe09b6cc7d2ac8df64d363ef3c88f3a66
André Rodrigues <andre360.ars@gmail.com>
2022-01-27 23:13:58 -0300

    Requisito 10 - Cria função que, retorna ficha do funcionario, com base em sua id, nome ou sobrenome, ou retorna a de todos, caso não se passe nenhum parametro

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const getEmployee = ({ name, id }) => employees
+  .find(({ firstName, lastName, id: employeeId }) => firstName === name
+    || lastName === name || employeeId === id);
+const getLocation = (animals) => animals.map((specie) => species
+  .find(({ id }) => specie === id).location);
+
+const getSpecies = (responsibleFor) => responsibleFor.map((specie) => species
+  .find(({ id }) => specie === id).name);
+
+const employeeCoverage = ({ firstName, lastName, id, responsibleFor }) => ({
+  id,
+  fullName: `${firstName} ${lastName}`,
+  species: getSpecies(responsibleFor),
+  locations: getLocation(responsibleFor),
+});
+
+function getEmployeesCoverage(options = undefined) {
+  const employeeInfo = [];
+
+  if (options && getEmployee(options) === undefined) {
+    throw new Error('Informações inválidas');
+  }
+
+  if (options) {
+    return employeeCoverage(getEmployee(options));
+  }
+
+  employees.forEach((employee) => {
+    employeeInfo.push(employeeCoverage(employee));
+  });
+
+  return employeeInfo;
 }
 
 module.exports = getEmployeesCoverage;
